54bd4d98947390356fb69e4c69232aa7c266aa11
fukudaum <fukudaum@gmail.com>
2022-06-14 14:27:03 -0300

    first commit

---
+++ funcoes.js
@@ -0,0 +1,9 @@
+function mostraIdade(nome, idade) {
+    return `A idade de ${nome} e ${idade}`;
+}
+
+function mostraCidade(nomeCidade) {
+    return `Minha cidade é ${nomeCidade}`;
+}
+
+export {mostraIdade, mostraCidade};
---
+++ main.js
@@ -0,0 +1,4 @@
+import { mostraIdade, mostraCidade } from './funcoes.js';
+
+console.log(mostraIdade('Gustavo', 38));
+console.log(mostraCidade('Maringá'));
